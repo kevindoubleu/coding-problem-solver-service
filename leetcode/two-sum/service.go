362ae9fe76259600b74f1bc6061e471444d2246f
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-02-10 12:56:41 +0700

    feat: add solve func

--- leetcode/two-sum/service.go
+++ leetcode/two-sum/service.go
@@ -1,6 +1,15 @@
 package twosum
 
 func solve(nums []int, target int) []int {
-	// unimplemented
-	return []int{1,2,3}
+	length := len(nums)
+
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			if nums[i]+nums[j] == target && i != j {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
 }
